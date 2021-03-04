5aa46dea68044d8b0ac5fbde628a955b862a4fa9
EunjaeJang <ejjang2030@gmail.com>
2021-03-04 18:13:59 +0900

    2021.03.04-18:09 : [두카드 뒤집기] 모듈화 1. 처음 스크립트 모듈화를 실행했을 때는 다음과 같은 에러가 뜸.   -- most likely due to a circular import   순환참조로 인한 에러가 발생   [해결]   1) 사이트 [https://medium.com/mathpresso/python-circular-imports-e89c5bf16510]   를 통해 원인을 파악하고 해결방법을 모색함.   2) 내가 해결한 방법 : execute.py 모듈을 만들어서 모든 스크립트 모듈에서 각 모듈의 메서드에 접근가능하도록 하였다.   그렇게 해도 순환참조 에러는 났음. 그래서 위 사이트의 방법대로 모듈 import 하는 순서를 메서드 로직 안에 넣었더니 해결됨.

---
+++ execute.py
@@ -0,0 +1,18 @@
+def initGame():
+    import initalGame
+    return initalGame.initGame()
+
+
+def selectGame():
+    import initalGame
+    return initalGame.selectGame()
+
+
+def initTwoCardFlipGame():
+    import twoCardFlipGame
+    return twoCardFlipGame.initTwoCardFlipGame()
+
+
+def runTwoCardFlipGame():
+    import twoCardFlipGame
+    return twoCardFlipGame.runTwoCardFlipGame()
